3a94422532834c583f7dd1c88064e57bdf8dfe34
Marçal <edumsfilho347@gmail.com>
2023-04-09 17:09:56 -0300

    estruturando a classe Loan

--- LibrarySystem/src/entities/Loan.java
+++ LibrarySystem/src/entities/Loan.java
@@ -1,4 +1,40 @@
 package entities;
+
+import java.time.LocalDate;
+
 public class Loan {
-    
+
+    private User user;
+    private Item item;
+    final private LocalDate loanDate = LocalDate.now();
+    final private LocalDate returnDate = loanDate.plusDays(45);
+
+    public Loan(User user, Item item) {
+        this.user = user;
+        this.item = item;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
 }
